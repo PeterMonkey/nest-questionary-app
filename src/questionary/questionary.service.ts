042846616bcaa1a62fff67b2bd77a9cbbf3dfeed
Pedro Fernandez <pefernandez1408@gmail.com>
2023-05-21 20:55:07 -0400

    ADD: Se creo el servicio para crear un cuestionario

--- src/questionary/questionary.service.ts
+++ src/questionary/questionary.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Questionary } from './questionary.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateQuestionaryDTO } from './dto/createQuestionary.dto';
 
 @Injectable()
-export class QuestionaryService {}
+export class QuestionaryService {
+  constructor(
+    @InjectRepository(Questionary)
+    private readonly questionaryRepository: Repository<Questionary>,
+  ) {}
+
+  async create(quest: CreateQuestionaryDTO): Promise<Questionary> {
+    const questionary = this.questionaryRepository.create({
+      title: quest.title,
+    });
+    await this.questionaryRepository.save(questionary);
+    return questionary;
+  }
+}
