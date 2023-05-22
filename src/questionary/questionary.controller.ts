05561cec9ae2e27bf73d9c6db6a14643ff0f7d83
Pedro Fernandez <pefernandez1408@gmail.com>
2023-05-21 21:36:21 -0400

    ADD: Se creo el controlador para crear un cuestionario

--- src/questionary/questionary.controller.ts
+++ src/questionary/questionary.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { QuestionaryService } from './questionary.service';
+import { CreateQuestionaryDTO } from './dto/createQuestionary.dto';
+import { Questionary } from './questionary.entity';
 
 @Controller('questionary')
-export class QuestionaryController {}
+export class QuestionaryController {
+  constructor(private questionaryService: QuestionaryService) {}
+
+  @Post()
+  createQuestionary(
+    @Body() createQuestionary: CreateQuestionaryDTO,
+  ): Promise<Questionary> {
+    return this.questionaryService.create(createQuestionary);
+  }
+}
