183031492340c875aabe1018a7b8ed844e8e7164
Pedro Fernandez <pefernandez1408@gmail.com>
2023-06-07 21:12:01 -0400

    ADD: Cracion del controlador create user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Post } from '@nestjs/common';
+import { CreateUserDTO } from './dto/createUser.dto';
+import { User } from './user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  create(@Body() createUser: CreateUserDTO): Promise<User> {
+    return this.userService.create(createUser);
+  }
+}
