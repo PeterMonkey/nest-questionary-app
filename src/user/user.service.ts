1b92e61d57775dec7ee6dee382af2a6d83df6b33
Pedro Fernandez <pefernandez1408@gmail.com>
2023-06-07 21:01:35 -0400

    ADD: Creacion del servicio create usuario

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDTO } from './dto/createUser.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(user: CreateUserDTO): Promise<User> {
+    try {
+      const { name, email, password } = user;
+      const data = this.userRepository.create({
+        name,
+        email,
+        password,
+      });
+
+      await this.userRepository.save(data);
+      return data;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
